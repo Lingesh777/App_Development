175ba7994d8edc6a345c80b61a561c81906c44df
Lingesh777 <727821tucs119@skct.edu.in>
2023-09-15 12:58:38 +0530

    log and sign

--- day 3/frontend/src/Signup/Signup.jsx
+++ day 3/frontend/src/Signup/Signup.jsx
@@ -2,6 +2,12 @@ import React, { useState } from 'react';
 import './Signup.css';
 import { Link, Navigate, useNavigate } from 'react-router-dom';
 import axios from "axios";
+import "react-toastify/dist/ReactToastify.css";
+import {toast,ToastContainer } from 'react-toastify';
+import PersonIcon from '@mui/icons-material/Person';
+import EmailIcon from '@mui/icons-material/Email';
+import LockIcon from '@mui/icons-material/Lock';
+import PhoneIcon from '@mui/icons-material/Phone';
 
 export const Signup = () => {
   const navigate=useNavigate();
@@ -16,7 +22,8 @@ export const Signup = () => {
     e.preventDefault();
     if(signup.username===""||signup.email===""||signup.password==="")
     {
-      console.log("Enter all fields");
+     // console.log("Enter all fields");
+      toast.error("Enter all fields");
     }
     else
     {
@@ -30,30 +37,59 @@ export const Signup = () => {
         console.log(res.data)
         if(res.data==='Signup success')
         {
-          navigate("/login");
+          toast.success(res.data);
+          setTimeout(()=>{
+            navigate("/login")
+        },2000);
+        }
+        else
+        {
+          toast.error(res.data);
         }
       })
     }
   }
   return (
+    <div className='signupbody'>
+      {/* <div className="stext">
+        <h3>Signup</h3>
+      </div> */}
     <div className='signup'>
       <div className='sbox'>
+    <ToastContainer
+    position="top-center"
+    autoClose={3000}
+    hideProgressBar={false}
+    newestOnTop={false}
+    closeOnClick
+    rtl={false}
+    pauseOnFocusLoss
+    draggable
+    pauseOnHover
+    theme="dark"
+  />
+  <h3 style={{fontWeight:'bold',fontSize:'30px'}}>SIGNUP</h3>
       <div className='sinput'>
+        <PersonIcon/>
         <input type='text' id='username' className='sin' required onChange={handleChange} placeholder='Username'></input>
       </div>
       <div className='sinput'>
+      `<EmailIcon/>
         <input type='email' id='email'className='sin' required onChange={handleChange} placeholder='Email'></input>
       </div>
       <div className='sinput'>
+        <LockIcon/>
         <input type='password' id='password' className='sin' required onChange={handleChange} placeholder='Password'></input>
       </div>
       <div className='sinput'>
+        <PhoneIcon/>
         <input type='text' id='mobilenum' className='sin' required onChange={handleChange} placeholder='Mobile Number'></input>
       </div>
       <div className='sinput'>
         <button className='sbutton' onClick={handleSubmit} >Register</button>
       </div>
-      <h4>Already an User ? <Link to='/login' style={{textDecoration:'none'}}>Login</Link></h4>
+      <h4>Already an User ? <span className='spannav' onClick={()=>navigate('/login')} style={{textDecoration:'none',color:'white'}}> Login</span></h4>
+    </div>
     </div>
     </div>
   )
