a33378eb06e343d8397d1eac0d178468ec4339b0
Lingesh777 <727821tucs119@skct.edu.in>
2023-09-20 12:40:40 +0530

    topbar altered

--- day 4/frontend/src/Bar/TopBar.jsx
+++ day 4/frontend/src/Bar/TopBar.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './TopBar.css';
 import LocalGroceryStoreIcon from '@mui/icons-material/LocalGroceryStore';
-import SearchIcon from '@mui/icons-material/Search';
-import PersonIcon from '@mui/icons-material/Person';
-import NotificationsIcon from '@mui/icons-material/Notifications';
+import SearchIcon from '@mui/icons-material/Search'
 import { Button } from '@mui/material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import CircleNotificationsIcon from '@mui/icons-material/CircleNotifications';
 export const TopBar = () => {
   return (
     <div className="topbar d-flex-r">
@@ -12,9 +12,9 @@ export const TopBar = () => {
         <div className='topname' ><h4 >Cary's</h4></div>
         <input type='text' placeholder='Type for Search' className='topsearch' style={{marginLeft:'20px',width:'15vw',height:'5vh',borderRadius:'5px',border:'none'}}></input>
         <div className='topsicon'><SearchIcon style={{fontSize:'25px'}}/></div>
-        <div className='toprev'><Button style={{color:'inherit',border:'2px solid white'}}>Review</Button></div>
-        <div className='topnoticon'><NotificationsIcon style={{fontSize:'25px'}}/></div>
-          <div className='topendicon'><PersonIcon style={{fontSize:'40px'}} /></div>
+        <div className='toprev'><Button style={{color:'inherit',border:'2px solid white',fontWeight:'bold'}}>Review</Button></div>
+          <div className='topnoticon'><CircleNotificationsIcon style={{fontSize:'40px'}}/></div>
+          <div className='topendicon'><AccountCircleIcon style={{fontSize:'40px'}} /></div>
     </div>
   )
 }
